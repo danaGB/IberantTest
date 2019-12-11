4695ff499630029c0aa3c212bb06b660f6841859
danaGB <d.gbeltran88@gmail.com>
2019-12-10 18:21:08 -0800

    Adding DTO and Model for Person

---
+++ src/PackingListApp/DTO/PersonDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PackingListApp.DTO
+{
+    public class PersonDTO
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Occupation { get; set; }
+    }
+}
---
+++ src/PackingListApp/Models/PersonModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PackingListApp.Models
+{
+    public class PersonModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Occupation { get; set; }
+    }
+}
